8020d8450ee568a7b2abc9622498bb303f96f60e
suah <christinekang0123@naver.com>
2021-07-01 17:17:00 +0900

    new project

--- app/src/main/java/com/example/madcamp1/MainActivity.java
+++ app/src/main/java/com/example/madcamp1/MainActivity.java
@@ -1,88 +1,86 @@
 package com.example.madcamp1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.content.res.AssetManager;
-import android.view.View;
-import android.widget.TextView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-
+import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private BottomNavigationView bottomNavigationView; // 바텀 네비게이션 뷰
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private ContactFrag cf;
+    private PhotoFrag pf;
+    private ExtraFrag ef;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> list = new ArrayList<>();
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-
-
-        AssetManager assetManager= getAssets();
-
-        try {
-            InputStream is= assetManager.open("json/phone.json");
-            InputStreamReader isr= new InputStreamReader(is);
-            BufferedReader reader= new BufferedReader(isr);
-
-            StringBuffer buffer= new StringBuffer();
-            String line= reader.readLine();
-            while (line!=null){
-                buffer.append(line+"\n");
-                line=reader.readLine();
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.contacts:
+                        setFrag(0);
+                        break;
+                    case R.id.photos:
+                        setFrag(1);
+                        break;
+                    case R.id.extra:
+                        setFrag(2);
+                        break;
+                }
+                return true;
             }
-            String jsonData= buffer.toString();
-            JSONArray jsonArray= new JSONArray(jsonData);
+        });
 
-            String s="";
+        cf = new ContactFrag();
+        pf = new PhotoFrag();
+        ef = new ExtraFrag();
 
-            for(int i=0; i<jsonArray.length();i++) {
-                JSONObject jo = jsonArray.getJSONObject(i);
-
-                String name = jo.getString("name");
-                String num = jo.getString("number");
-
-
-                s = name + "\t" + num ;
-                list.add(s);
-            }
-
-        } catch (IOException e) {e.printStackTrace();} catch (JSONException e) {e.printStackTrace(); }
-
-        RecyclerView recyclerView = findViewById(R.id.recycler1) ;
-        recyclerView.setLayoutManager(new LinearLayoutManager(this)) ;
-
-        ViewAdapter adapter = new ViewAdapter(list) ;
-        recyclerView.setAdapter(adapter) ;
-
+        setFrag(0); // 첫 프래그먼트 화면 지정
     }
 
+    // 프레그먼트 교체
+    private void setFrag(int n)
+    {
+        fm = getSupportFragmentManager();
+        ft= fm.beginTransaction();
+        switch (n)
+        {
+            case 0:
+                ft.replace(R.id.Main_Frame,cf);
+                ft.commit();
+                break;
+
+            case 1:
+                ft.replace(R.id.Main_Frame,pf);
+                ft.commit();
+                break;
+
+            case 2:
+                ft.replace(R.id.Main_Frame,ef);
+                ft.commit();
+                break;
+        }
+    }
 }
